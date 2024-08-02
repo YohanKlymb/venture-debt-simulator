ed72028727c9aa283a1986955dee413bc69d777c
yohan <yohan.obadia@gmail.com>
2024-08-02 16:40:32 +0200

    trying new chart dimensions

--- js/main.js
+++ js/main.js
@@ -590,8 +590,8 @@ document.addEventListener("DOMContentLoaded", function() {
         // layout.margin = {l: 20, r: 10, b: 10, t: 20, pad: 2};
         layout.autosize = false;
         const dim = document.querySelector('.chart-div').getBoundingClientRect()
-        layout.width = 400//dim.width * 0.9;
-        layout.height = 400//dim.height * 0.9;
+        layout.width = dim.width * 0.9;
+        // layout.height = 400//dim.height * 0.9;
 
         // Check if the chart has been rendered before by checking for existing data
         if (!chartElement.data || chartElement.data.length === 0 || onlyRender) {
